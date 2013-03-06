c7da7784ec286e8476482898d669cbd4db159080
Phillip Kast <kastph@gmail.com>
2013-03-05 19:48:08 -0800

    Modify SimpleImageFilter example to repro a crash.
    - Strip out most of the example
    - Perform a single, resample using GPUImageLanczosResamplingFilter and
      imageByFilteringImage:

--- examples/SimpleImageFilter/SimpleImageFilter/SimpleImageViewController.h
+++ examples/SimpleImageFilter/SimpleImageFilter/SimpleImageViewController.h
@@ -9,11 +9,7 @@
     UISlider *imageSlider;
 }
 
-// Image filtering
-- (void)setupDisplayFiltering;
-- (void)setupImageFilteringToDisk;
 - (void)setupImageResampling;
 
-- (IBAction)updateSliderValue:(id)sender;
 
 @end
